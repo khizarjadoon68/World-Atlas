a0c9babd2d0e9dfa06d48a22c401613cb6abbf15
Khizar jadoon <khizarjadoon68@gmail.com>
2025-09-05 12:46:10 +0500

    add responsive

--- src/Components/UI/Header.jsx
+++ src/Components/UI/Header.jsx
@@ -4,22 +4,22 @@ const Header = () => {
     return (
         <>
             <header>
-                <section>
-                    <div className="bg-neutral-800 text-white w-[70%] h-15 flex justify-around items-center ">
-                        <div className="">
+                <section className="bg-neutral-800 text-white w-full">
+                    <div className="container mx-auto px-4 py-4 flex flex-col sm:flex-row justify-between items-center">
+                        <div>
                             <NavLink to="/">
                                 <h1 className="text-3xl font-bold" >WorldAtlas</h1>
                             </NavLink >
 
                         </div>
-                        <div className="">
-                            <ul className="flex gap-5 cursor-pointer">
+                        <nav className="mt-4 sm:mt-0">
+                            <ul className="flex flex-col sm:flex-row items-center gap-4 sm:gap-5 cursor-pointer">
                                 <li> <NavLink to="/">Home</NavLink></li>
                                 <li> <NavLink to="about">About</NavLink></li>
                                 <li> <NavLink to="country">Country</NavLink></li>
                                 <li> <NavLink to="contact">Contact</NavLink></li>
                             </ul>
-                        </div>
+                        </nav>
                     </div>
                 </section>
             </header>
